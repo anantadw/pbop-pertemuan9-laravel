7d893468f5b8b60c034e1ec889e29cae596fb7b2
Ananta Destawardhana <anantadesta.public@gmail.com>
2022-11-03 08:53:31 +0700

    membuat migrasi dan model untuk tabel book

---
+++ database/migrations/2022_11_03_015054_create_books_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('judul')->unique();
+            $table->string('pengarang');
+            $table->string('penerbit');
+            $table->string('tahun_terbit');
+            $table->integer('jumlah_buku');
+            $table->text('deskripsi');
+            $table->string('gambar');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
